08b5ff4362d429dbd4ab3c7e1d9ca44d58742cf8
David Durant <dave@bowsy.co.uk>
2025-06-19 02:49:13 +0100

    So much faff moving to python

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,12 @@
+import sys
+import os
+
+# Add the project directory and site-packages from the virtual environment
+sys.path.insert(0, '/var/www/ukgovcomms')
+sys.path.insert(0, '/var/www/.venvs/ukgovcomms/lib/python3.6/site-packages')  # adjust version if needed
+
+# Set environment variable so Flask knows where the app is
+os.environ['FLASK_APP'] = 'app'
+
+# Import and expose the Flask application
 from app import app as application
